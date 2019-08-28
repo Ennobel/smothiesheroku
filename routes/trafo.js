204d6b2dbbcc595b775ee275cce6a32f3ac7240d
Ennobel <ennobell17@gmail.com>
2019-08-28 13:35:18 +0700

    exlude router api

--- routes/trafo.js
+++ routes/trafo.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/',
-  require("../controller/index").show
+router.get('/get_location',
+  require("../controller/trafo").getLocation
 );
 
 module.exports = router;
