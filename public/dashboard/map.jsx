d4b072a98b7b6f0ced8a8ff38b9a9bfb7399c068
Ennobel <ennobell17@gmail.com>
2019-10-04 20:07:05 +0700

    install react dom

--- public/dashboard/map.jsx
+++ public/dashboard/map.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 export default class Map extends React.Component{
     render(){
         const position = [51.505, -0.09]
         return(
             <Map center={position} zoom={13}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Marker position={position}>
-      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
-  </Map>
-            )
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+                />
+                <Marker position={position}>
+                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                </Marker>
+            </Map>
+        )
     }    
 }
