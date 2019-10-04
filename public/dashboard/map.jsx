49115d79b3d873a2c49183f903d8f6fb9f7ab3b7
Ennobel <ennobell17@gmail.com>
2019-10-04 20:13:16 +0700

    install scirpt

--- public/dashboard/map.jsx
+++ public/dashboard/map.jsx
@@ -6,6 +6,8 @@ export default class Map extends React.Component{
         const position = [51.505, -0.09]
         return(
             <Map center={position} zoom={13}>
+                <script src="https://unpkg.com/react-leaflet/dist/react-leaflet.min.js"></script>
+                <script src="https://unpkg.com/react-leaflet/dist/react-leaflet.js"></script>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
