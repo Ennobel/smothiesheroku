3350fa0a0a61d7650b82ac4a6e127be3c54bc210
Ennobel <ennobell17@gmail.com>
2019-10-04 14:26:24 +0700

    solve confict

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-<<<<<<< HEAD
 var adminBroRouter = require('./routes/admin');
-=======
 var trafoRouter = require('./routes/trafo');
->>>>>>> bf2687c2d460e315d018582122b906bb2b5029f2
 
 var app = express();
 
@@ -26,11 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/admin', adminBroRouter);
-=======
 app.use('/api/v1/trafo',trafoRouter);
->>>>>>> bf2687c2d460e315d018582122b906bb2b5029f2
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
