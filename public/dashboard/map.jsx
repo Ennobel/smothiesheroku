3ff0a65a98e6ae4393b6a3a9934b523b381258d4
Ennobel <ennobell17@gmail.com>
2019-10-04 19:34:37 +0700

    coba dashboard

--- public/dashboard/map.jsx
+++ public/dashboard/map.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default class Map extends React.Component{
     render(){
         return(
-            <h1>Hello, world!</h1>
+            <div>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+            <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+            <script src="https://unpkg.com/esri-leaflet/dist/esri-leaflet.js"></script>
+            <div id="map">  </div>
+          <script type="text/javascript">
+          </script>
+          <script src="javascripts/map.js"></script></div>
             )
     }    
 }
