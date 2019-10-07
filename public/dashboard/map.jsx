a58bb55124f7b3e125d1efd5ccec29391ca448d6
Ennobel <ennobell17@gmail.com>
2019-10-07 12:37:31 +0700

    coba map

--- public/dashboard/map.jsx
+++ public/dashboard/map.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import 'https://unpkg.com/leaflet/dist/leaflet.css';
 export default class Map extends React.Component{
+    componentDidMount () {
+        const leaflet = document.createElement("script");
+        const map = document.createElement("script");
+        const esri = document.createElement("script");
+    
+        leaflet.src = "https://unpkg.com/leaflet/dist/leaflet.js";
+        leaflet.async = true;
+    
+        esri.src = "https://unpkg.com/leaflet/dist/leaflet.js";
+        esri.async = true;
+    
+        map.src = "javascripts/map.js";
+        map.async = true;
+    
+        document.body.appendChild(leaflet);
+        document.body.appendChild(esri);
+        document.body.appendChild(map);
+    }
     render(){
         return(
-            <h1>COMING SOON</h1>
+            <div id="map">
+                
+            </div>
         )
     }    
 }
