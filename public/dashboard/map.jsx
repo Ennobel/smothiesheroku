230515f62ad90ee324bfdfdc2c48cf68f1650f99
Ennobel <37362828+Ennobel@users.noreply.github.com>
2019-10-14 21:17:35 +0700

    Update map.jsx

--- public/dashboard/map.jsx
+++ public/dashboard/map.jsx
@@ -5,26 +5,26 @@ export default class Map extends React.Component{
         const leaflet = document.createElement("script");
         const map = document.createElement("script");
         const esri = document.createElement("script");
+        const boost = document.createElement("script");
     
+        boost.src = "https://unpkg.com/react-dom/umd/react-dom.production.min.js";
+        boost.async = true;
+        
         leaflet.src = "https://unpkg.com/leaflet/dist/leaflet.js";
-        leaflet.async = false;
+        leaflet.async = true;
     
         esri.src = "https://unpkg.com/leaflet/dist/leaflet.js";
-        esri.async = false;
+        esri.async = true;
     
         map.src = "javascripts/map.js";
         map.async = true;
     
+        document.body.appendChild(boost);
         document.body.appendChild(leaflet);
         document.body.appendChild(esri);
         document.body.appendChild(map);
     }
     render(){
-        var mapsytle = { 
-           position: 'absolute',
-           top: 0,
-           bottom: 0
-        }
         return(
             <div id="map" class="col-xs-12 col-sm-9  margin-zero map-padding" style={{height : '100%'}}>
 
